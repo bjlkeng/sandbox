12abdb8157cb749cdb8ecef26c5e4c1dc8fd3d2c
Brian Keng <brian.keng@gmail.com>
2018-08-24 09:43:56 -0400

    updating to use preprocessing, no dropout.

--- notebooks/label_refinery/run.sh
+++ notebooks/label_refinery/run.sh
@@ -3,15 +3,16 @@ export CMDLINE=1
 export OUTDIR="output-$(date +'%s')"
 mkdir -p $OUTDIR
 
-for dropout in 0.0 0.1 0.2; do
+for use_preprocessing in 0 1; do
     for run_num in 1 2 3 4 5; do
-        echo "Dropout=$dropout, RUN=$run_num"
-        export DROPOUT="$dropout"
+        echo "USE_PREPROCESSING=$use_preprocessing, RUN=$run_num"
+        export USE_PREPROCESSING="$use_preprocessing"
         export RUN_NUM="$run_num"
-        cp label_refinery{,-dropout${dropout}-run_num${RUN_NUM}}.ipynb
-        jupyter nbconvert --to notebook --execute label_refinery-dropout${dropout}-run_num${RUN_NUM}.ipynb --ExecutePreprocessor.timeout=-1
-        rm label_refinery-dropout${dropout}-run_num${RUN_NUM}.ipynb
-        mv label_refinery-dropout${dropout}-run_num${RUN_NUM}.nbconvert.ipynb $OUTDIR
+        cp label_refinery{,-use_pre${use_preprocessing}-run_num${RUN_NUM}}.ipynb
+        jupyter nbconvert --to notebook --execute label_refinery-use_pre${use_preprocessing}-run_num${RUN_NUM}.ipynb --ExecutePreprocessor.timeout=-1
+        rm label_refinery-use_pre${use_preprocessing}-run_num${RUN_NUM}.ipynb
+        mv label_refinery-use_pre${use_preprocessing}-run_num${RUN_NUM}.nbconvert.ipynb $OUTDIR
         mv results* $OUTDIR
     done
 done
+mv allresults.csv $OUTDIR
