dabc1a7a5a09dfd643e286ca89d57d52ef0fad95
Brian Keng <brian.keng@gmail.com>
2018-09-01 11:45:08 -0400

    Adding runs for label refinery.

--- notebooks/label_refinery/run.sh
+++ notebooks/label_refinery/run.sh
@@ -1,6 +1,8 @@
 export CMDLINE=1
+# PREFIX="label_refinery"
+PREFIX="label_refinery-cifar10"
 
-export OUTDIR="output-$(date +'%s')"
+export OUTDIR="output-$(date +'%s')-$PREFIX"
 mkdir -p $OUTDIR
 
 for run_num in 1 2 3 4 5; do
@@ -8,10 +10,10 @@ for run_num in 1 2 3 4 5; do
         echo "USE_PREPROCESSING=$use_preprocessing, RUN=$run_num"
         export USE_PREPROCESSING="$use_preprocessing"
         export RUN_NUM="$run_num"
-        cp label_refinery{,-use_pre${use_preprocessing}-run_num${RUN_NUM}}.ipynb
-        jupyter nbconvert --to notebook --execute label_refinery-use_pre${use_preprocessing}-run_num${RUN_NUM}.ipynb --ExecutePreprocessor.timeout=-1
-        rm label_refinery-use_pre${use_preprocessing}-run_num${RUN_NUM}.ipynb
-        mv label_refinery-use_pre${use_preprocessing}-run_num${RUN_NUM}.nbconvert.ipynb $OUTDIR
+        cp $PREFIX{,-use_pre${use_preprocessing}-run_num${RUN_NUM}}.ipynb
+        jupyter nbconvert --to notebook --execute ${PREFIX}-use_pre${use_preprocessing}-run_num${RUN_NUM}.ipynb --ExecutePreprocessor.timeout=-1
+        rm ${PREFIX}-use_pre${use_preprocessing}-run_num${RUN_NUM}.ipynb
+        mv ${PREFIX}-use_pre${use_preprocessing}-run_num${RUN_NUM}.nbconvert.ipynb $OUTDIR
         mv results* $OUTDIR
     done
 done
